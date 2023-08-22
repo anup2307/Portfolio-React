243c0dd936f0de75f79c81760603e240c3cf43ac
anup2307 <124316722+anup2307@users.noreply.github.com>
2023-08-22 15:54:00 -0400

    fixing resume file location error

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div className="h-screen bg-[#21325e] flex flex-col justify-center items-center">
       <div className="fadeInRight">
-        <h1 className=' p-0 mb-8 text-2xl text-white'>Download <a href='/AnuPriyaGovindarajan.docx' download target="_blank" className="border-b-4 border-pink-600 font-bold">My Resume</a></h1>
+        <h1 className=' p-0 mb-8 text-2xl text-white'>Download <a href='/public/AnuPriyaGovindarajan.docx' download target="_blank" className="border-b-4 border-pink-600 font-bold">My Resume</a></h1>
         <h2 className='inline border-b-4 border-pink-600 text-2xl text-white font-bold '> Skills </h2>
         <div className='w-full flex flex-row mt-3'>
           <div className='sm:text-right text-2xl '>
